817730d49d5ddc84e111b453344c8624fb251789
Fraser King <frasertheking@gmail.com>
2016-12-01 19:39:45 -0500

    Initial Commit

---
+++ CustomLoadingAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLoadingAnimation
+//
+//  Created by Fraser King on 2016-12-01.
+//  Copyright © 2016 Fraser King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomLoadingAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomLoadingAnimation
+//
+//  Created by Fraser King on 2016-12-01.
+//  Copyright © 2016 Fraser King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
