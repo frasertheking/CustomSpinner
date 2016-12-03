da13d5670693977bf7395760793f5808b4922dd7
Fraser King <frasertheking@gmail.com>
2016-12-02 20:19:44 -0500

    initial commit

--- CustomLoadingAnimation/ViewController.m
+++ CustomLoadingAnimation/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+animatedGIF.h"
 
 @interface ViewController ()
+
+@property (nonatomic) IBOutlet UIImageView *backgroundImage;
+@property (nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic) IBOutlet UIImageView *imageViewBackground;
+@property (nonatomic) NSMutableArray *imageArray;
+@property (nonatomic) NSInteger count;
+@property (nonatomic) UIImage *gif;
 
 @end
 
@@ -16,13 +24,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imageArray = [[NSMutableArray alloc] initWithObjects:@"puppy", @"cat", @"duck", @"trump", nil];
+    self.count = 0;
+    [self changePic];
+    
+    self.gif = [UIImage animatedImageWithAnimatedGIFURL:[NSURL URLWithString:@"https://frasertheking.com/images/newgif2.gif"]];
+    self.imageView.image = self.gif;
+    [NSTimer scheduledTimerWithTimeInterval:5.0f target:self selector:@selector(changePic) userInfo:nil repeats:YES];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)changePic {
+    self.backgroundImage.image = [UIImage imageNamed:self.imageArray[self.count]];
+    if (self.count == 3) {
+        self.count = 0;
+        [UIView animateWithDuration:1 animations:^{
+            self.imageViewBackground.alpha = 1;
+            self.imageView.alpha = 0;
+        }];
+    } else {
+        self.count++;
+    }
 }
 
 
